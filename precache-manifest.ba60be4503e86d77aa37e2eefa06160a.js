33f99fd3077c0f694c6f61b44a396921e0c0be15
polak@cngroup.dk <polak@cngroup.dk>
2020-12-02 09:00:48 +0100

    Updates

---
+++ precache-manifest.ba60be4503e86d77aa37e2eefa06160a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58943aa751385e76fe72e8f235330684",
+    "url": "/mobx-react-ts-todo/index.html"
+  },
+  {
+    "revision": "ddfd008d016ce936cb0b",
+    "url": "/mobx-react-ts-todo/static/css/main.91343ceb.chunk.css"
+  },
+  {
+    "revision": "1ccffd8ce9197fc33d59",
+    "url": "/mobx-react-ts-todo/static/js/2.d74573be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mobx-react-ts-todo/static/js/2.d74573be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddfd008d016ce936cb0b",
+    "url": "/mobx-react-ts-todo/static/js/main.c5ab6248.chunk.js"
+  },
+  {
+    "revision": "3a2f81869f02be4f0ea0",
+    "url": "/mobx-react-ts-todo/static/js/runtime-main.3f91fe9c.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/mobx-react-ts-todo/precache-manifest.ba60be4503e86d77aa37e2eefa06160a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mobx-react-ts-todo/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
